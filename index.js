9346d0231e626a3004ad9dc353efae3b0fe907a4
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-01-29 15:28:55 +0200

    add event listener and API info

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const btnEl = document.getElementById("btn");
 const animeContainerEl = document.querySelector(".anime-container");
 const imgEl = document.querySelector(".anime-img");
 const animeName = document.querySelector(".anime-name");
+
+//Add Event Listener
