f39abecab788197e52cbff397639e414c4758b80
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-01-29 07:07:56 +0200

    DOM manipulation - get html elements

---
+++ index.js
@@ -0,0 +1,6 @@
+//DOM Manipulation
+//Get Elements
+const btnEl = document.getElementById("btn");
+const animeContainerEl = document.querySelector(".anime-container");
+const imgEl = document.querySelector(".anime-img");
+const animeName = document.querySelector(".anime-name");
