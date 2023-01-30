b98c68c7411bb0b1a9d0695512c75f22881a9784
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-01-30 16:34:05 +0200

    set up button and display logic

--- index.js
+++ index.js
@@ -2,7 +2,29 @@
 //Get Elements
 const btnEl = document.getElementById("btn");
 const animeContainerEl = document.querySelector(".anime-container");
-const imgEl = document.querySelector(".anime-img");
+const animeImgEl = document.querySelector(".anime-img");
 const animeName = document.querySelector(".anime-name");
 
 //Add Event Listener
+btnEl.addEventListener("click", async () => {
+  //API google cat boys api
+  //www.catboys.com
+  //No api key needed
+
+  try {
+    btnEl.disabled = true;
+    btnEl.innerText = "loading...";
+    //fetch API
+    const response = await fetch("https://api.catboys.com/img");
+    //convert to json
+    const data = await response.json();
+    console.log(data);
+    animeContainerEl.style.display = "block";
+    animeImgEl.src = data.url;
+    animeName.innerText = data.artist;
+    btnEl.disabled = false;
+    btnEl.innerText = "Get Anime";
+  } catch (error) {
+    console.log(error);
+  }
+});
